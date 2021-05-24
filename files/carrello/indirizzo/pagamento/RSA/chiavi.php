025e559cc1db8928e1cce45c5442b9b6d0b17bb9
Culcuz <76112998+Culcuz@users.noreply.github.com>
2021-05-24 17:37:51 +0200

    Migliorata grafica.

--- files/carrello/indirizzo/pagamento/RSA/chiavi.php
+++ files/carrello/indirizzo/pagamento/RSA/chiavi.php
@@ -104,114 +104,173 @@
             <!-- End Navbar -->
             <div class="content">
                 <div class="container-fluid">
+                    <!-- INIZIO RSA -->
                     <?php
                     include 'phpseclib/Crypt/RSA.php';
-                    if (!isset($_SESSION['connessione'])) {
+
+                    $numero = $_POST['numero'];
+                    $nome = $_POST['nome'];
+                    $mese = $_POST['mese'];
+                    $anno = $_POST['anno'];
+                    $cvc = $_POST['cvc'];
+
+
+                    $rsa = new Crypt_RSA();
+                    extract($rsa->createKey());
+
+                    $plaintext = "$numero-$nome-$mese-$anno-$cvc";
+
+                    $rsa->loadKey($privatekey);
+                    $ciphertext = $rsa->encrypt($plaintext);
+                    $rsa->loadKey($publickey);
+                    $decriptato = $rsa->decrypt($ciphertext);
                     ?>
-                        <script>
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Ops...',
-                                text: 'Non sei autorizzato',
-                                allowEscape: false,
-                                allowOutsideClick: false,
-                                confirmButtonText: "<a href='../../../index.php'>Esci</a>"
-                            });
-                        </script>
-                    <?php
-                    } else {
-                        $numero = $_POST['numero'];
-                        $nome = $_POST['nome'];
-                        $mese = $_POST['mese'];
-                        $anno = $_POST['anno'];
-                        $cvc = $_POST['cvc'];
+                    <!-- FINE RSA -->
 
 
-                        $rsa = new Crypt_RSA();
-                        extract($rsa->createKey());
-                        echo $privatekey;
-                        echo "<br>";
-                        echo $publickey;
 
-                        $plaintext = "$numero-$nome-$mese-$anno-$cvc";
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="card">
+                                <div class="card-header card-header-success">
+                                    <h4 class="card-title">Chiavi</h4>
+                                </div>
+                                <div class="card-body table-responsive">
+                                    <table class="table table-hover">
 
-                        $rsa->loadKey($privatekey);
-                        $ciphertext = $rsa->encrypt($plaintext);
-                        echo "<br>";
-                        echo $ciphertext;
-                        echo "<br>";
-                        $rsa->loadKey($publickey);
-                        echo $rsa->decrypt($ciphertext);
-                    }
-                    ?>
+                                        <tbody>
+                                            <tr class="text-center">
+                                                <td>
+                                                    <strong>Chiave Privata </strong>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?php echo $privatekey; ?></td>
+                                            </tr>
+                                            <tr class="text-center">
+                                                <td>
+                                                    <strong>Chiave Pubblica </strong>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?php echo $publickey; ?></td>
+                                            </tr>
 
-                </div>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="card">
+                                <div class="card-header card-header-danger">
+                                    <h4 class="card-title">Risultati</h4>
+                                </div>
+                                <div class="card-body table-responsive">
+                                    <table class="table table-hover">
 
-                <footer class="footer">
-                    <div class="container-fluid">
-                        <nav class="float-left">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        About Me
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                        <div class="copyright float-right">
-                            &copy;
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script>
-                            by <strong> Riccardo Delfrate</strong>
+                                        <tbody>
+                                            <tr class="text-center">
+                                                <td>
+                                                    <strong>Testo in chiaro </strong>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?php echo $plaintext; ?></td>
+                                            </tr>
+                                            <tr class="text-center">
+                                                <td>
+                                                    <strong>Testo criptato </strong>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?php echo $ciphertext; ?></td>
+                                            </tr>
+                                            <tr class="text-center">
+                                                <td>
+                                                    <strong>Testo decriptato </strong>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?php echo $decriptato; ?></td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </footer>
+                </div>
+
+
             </div>
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <nav class="float-left">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    About Me
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="copyright float-right">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        by <strong> Riccardo Delfrate</strong>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        <!--   Core JS Files   -->
-        <script src="../../../../../assets/js/core/jquery.min.js"></script>
-        <script src="../../../../../assets/js/core/popper.min.js"></script>
-        <script src="../../../../../assets/js/core/bootstrap-material-design.min.js"></script>
-        <script src="../../../../../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-        <!-- Plugin for the momentJs  -->
-        <script src="../../../../../assets/js/plugins/moment.min.js"></script>
-        <!--  Plugin for Sweet Alert -->
-        <script src="../../../../../assets/js/plugins/sweetalert2.js"></script>
-        <!-- Forms Validations Plugin -->
-        <script src="../../../../../assets/js/plugins/jquery.validate.min.js"></script>
-        <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-        <script src="../../../../../assets/js/plugins/jquery.bootstrap-wizard.js"></script>
-        <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-        <script src="../../../../../assets/js/plugins/bootstrap-selectpicker.js"></script>
-        <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
-        <script src="../../../../../assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
-        <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
-        <script src="../../../../../assets/js/plugins/jquery.dataTables.min.js"></script>
-        <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-        <script src="../../../../../assets/js/plugins/bootstrap-tagsinput.js"></script>
-        <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-        <script src="../../../assets/js/plugins/jasny-bootstrap.min.js"></script>
-        <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-        <script src="../../..//js/plugins/fullcalendar.min.js"></script>
-        <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-        <script src="../../../../../assets/js/plugins/jquery-jvectormap.js"></script>
-        <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-        <script src="../../../../../assets/js/plugins/nouislider.min.js"></script>
-        <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-        <!-- Library for adding dinamically elements -->
-        <script src="../../../../../assets/js/plugins/arrive.min.js"></script>
-        <!--  Google Maps Plugin    -->
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-        <!-- Chartist JS -->
-        <script src="../../../../../assets/js/plugins/chartist.min.js"></script>
-        <!--  Notifications Plugin    -->
-        <script src="../../../../../assets/js/plugins/bootstrap-notify.js"></script>
-        <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-        <script src="../../../../../assets/js/material-dashboard.js?v=2.1.2" type="text/javascript"></script>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <!--   Core JS Files   -->
+    <script src="../../../../../assets/js/core/jquery.min.js"></script>
+    <script src="../../../../../assets/js/core/popper.min.js"></script>
+    <script src="../../../../../assets/js/core/bootstrap-material-design.min.js"></script>
+    <script src="../../../../../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!-- Plugin for the momentJs  -->
+    <script src="../../../../../assets/js/plugins/moment.min.js"></script>
+    <!--  Plugin for Sweet Alert -->
+    <script src="../../../../../assets/js/plugins/sweetalert2.js"></script>
+    <!-- Forms Validations Plugin -->
+    <script src="../../../../../assets/js/plugins/jquery.validate.min.js"></script>
+    <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+    <script src="../../../../../assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+    <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+    <script src="../../../../../assets/js/plugins/bootstrap-selectpicker.js"></script>
+    <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+    <script src="../../../../../assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+    <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+    <script src="../../../../../assets/js/plugins/jquery.dataTables.min.js"></script>
+    <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+    <script src="../../../../../assets/js/plugins/bootstrap-tagsinput.js"></script>
+    <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+    <script src="../../../assets/js/plugins/jasny-bootstrap.min.js"></script>
+    <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+    <script src="../../..//js/plugins/fullcalendar.min.js"></script>
+    <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+    <script src="../../../../../assets/js/plugins/jquery-jvectormap.js"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="../../../../../assets/js/plugins/nouislider.min.js"></script>
+    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+    <!-- Library for adding dinamically elements -->
+    <script src="../../../../../assets/js/plugins/arrive.min.js"></script>
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Chartist JS -->
+    <script src="../../../../../assets/js/plugins/chartist.min.js"></script>
+    <!--  Notifications Plugin    -->
+    <script src="../../../../../assets/js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../../../../../assets/js/material-dashboard.js?v=2.1.2" type="text/javascript"></script>
 
 
 </body>
