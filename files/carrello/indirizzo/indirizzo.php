36988ce7e1f9acb217e880d773f865bdf627435e
Culcuz <76112998+Culcuz@users.noreply.github.com>
2021-05-27 19:25:30 +0200

    Modificato form spedizione indirizzo.php

--- files/carrello/indirizzo/indirizzo.php
+++ files/carrello/indirizzo/indirizzo.php
@@ -86,8 +86,7 @@
                     <a class='simple-text logo-normal'>
                     Benvenuta/o $nome
                     </a>
-                    </div>"
-                ;
+                    </div>";
             }
             ?>
 
@@ -107,8 +106,7 @@
                             <i class='material-icons'>person</i>
                             <p>Accedi/Registrati</p>
                             </a>
-                            </li>"
-                        ;
+                            </li>";
                     }
                     ?>
                     <li class="nav-item ">
@@ -125,8 +123,7 @@
                             <i class='material-icons'>logout</i>
                             <p>Logout</p>
                             </a>
-                            </li>"
-                        ;
+                            </li>";
                     }
                     ?>
 
@@ -162,6 +159,109 @@
             </nav>
             <div class="content">
                 <div class="container-fluid">
+                <!-- INIZIO INSERIMENTO INDIRIZZO NUOVO-->
+                <link href="../../css/main.css" rel="stylesheet" media="all">
+                    <div class="col-lg-4 col-md-12">
+                        <div class="wrapper wrapper--w790">
+                            <div class="card card-5">
+                                <div class="card-heading">
+                                    <h2 class="title">Event Registration Form</h2>
+                                </div>
+                                <div class="card-body">
+                                    <form method="POST">
+                                        <div class="form-row m-b-55">
+                                            <div class="name">Name</div>
+                                            <div class="value">
+                                                <div class="row row-space">
+                                                    <div class="col-2">
+                                                        <div class="input-group-desc">
+                                                            <input class="input--style-5" type="text" name="first_name">
+                                                            <label class="label--desc">first name</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-2">
+                                                        <div class="input-group-desc">
+                                                            <input class="input--style-5" type="text" name="last_name">
+                                                            <label class="label--desc">last name</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="name">Company</div>
+                                            <div class="value">
+                                                <div class="input-group">
+                                                    <input class="input--style-5" type="text" name="company">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="name">Email</div>
+                                            <div class="value">
+                                                <div class="input-group">
+                                                    <input class="input--style-5" type="email" name="email">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row m-b-55">
+                                            <div class="name">Phone</div>
+                                            <div class="value">
+                                                <div class="row row-refine">
+                                                    <div class="col-3">
+                                                        <div class="input-group-desc">
+                                                            <input class="input--style-5" type="text" name="area_code">
+                                                            <label class="label--desc">Area Code</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-9">
+                                                        <div class="input-group-desc">
+                                                            <input class="input--style-5" type="text" name="phone">
+                                                            <label class="label--desc">Phone Number</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="name">Subject</div>
+                                            <div class="value">
+                                                <div class="input-group">
+                                                    <div class="rs-select2 js-select-simple select--no-search">
+                                                        <select name="subject">
+                                                            <option disabled="disabled" selected="selected">Choose option</option>
+                                                            <option>Subject 1</option>
+                                                            <option>Subject 2</option>
+                                                            <option>Subject 3</option>
+                                                        </select>
+                                                        <div class="select-dropdown"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row p-t-20">
+                                            <label class="label label--block">Are you an existing customer?</label>
+                                            <div class="p-t-15">
+                                                <label class="radio-container m-r-55">Yes
+                                                    <input type="radio" checked="checked" name="exist">
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                                <label class="radio-container">No
+                                                    <input type="radio" name="exist">
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <!-- FINE INSERIMENTO INDIRIZZO NUOVO-->
+
                     <div class="row">
                         <form action="./pagamento/pagamento.php" method="post">
                             <div class="col-lg-4 col-md-12">
